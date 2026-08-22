c558eb44ae42a2b1c4aea920939d0a3ed52c76b0
chandannial415-debug <chandannial415@gmail.com>
2026-08-22 13:34:02 +0530

    Update supabase.ts

--- artifacts/matric-notes/src/data/supabase.ts
+++ artifacts/matric-notes/src/data/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = 'https://g1x5OeugaZ6vcyxMkCwwDA.supabase.co';
-const supabaseAnonKey = 'Sb_publishable_g1x5OeugaZ6vcyxMkCwwDA_jkhRWGDf';
+const supabaseUrl = '';
+const supabaseAnonKey = 'sb_publishable_g1x5OeugaZ6vcyxMkCwwDA_jkhRWGDf';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
